aab5f9f7dab2f46dda8ee21634f06fe57e4e5695
singhabhay25540-alt <singhabhay25540@gmail.com>
2026-07-23 13:18:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.express as px
 
 # ---------------- PAGE SETTINGS ---------------- #
 st.set_page_config(page_title="IPL Data Analysis Dashboard",
